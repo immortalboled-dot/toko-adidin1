4fed5a3eccf1e5d5e2566f409f8c738564ad2bae
immortalboled-dot <immortalboled@gmail.com>
2026-08-04 20:37:32 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('PWA Service Worker terinstall');
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
